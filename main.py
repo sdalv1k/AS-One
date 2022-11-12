777cb3c8e5598afe449c2edaafdd4867f19efb2a
Sindre Frantzen Dalvik <sdalv1k@uit.no>
2022-11-12 16:45:29 +0100

    Track using webcam

--- main.py
+++ main.py
@@ -13,25 +13,24 @@ def main(args):
         detector=asone.YOLOX_DARKNET_PYTORCH,
         use_cuda=args.use_cuda
         )
-    # Get tracking function
-    track_fn = dt_obj.track_video(args.video_path,
-                                output_dir=args.output_dir,
-                                save_result=args.save_result,
-                                display=args.display,
-                                draw_trails=args.draw_trails,
-                                filter_classes=filter_classes)
+
+    # Track using webcam
+    track_fn = dt_obj.track_webcam(cam_id=0, output_dir='data/results', save_result=False, display=True)
     
     # Loop over track_fn to retrieve outputs of each frame 
     for bbox_details, frame_details in track_fn:
         bbox_xyxy, ids, scores, class_ids = bbox_details
         frame, frame_num, fps = frame_details
-        print(frame_num)
+
+        if 0 in class_ids:
+            # It's a human
+            pass
         
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('video_path', help='Path to input video')
+    parser.add_argument('--video_path', help='Path to input video')
     parser.add_argument('--cpu', default=True, action='store_false', dest='use_cuda',
                         help='run on cpu if not provided the program will run on gpu.')
     parser.add_argument('--no_save', default=True, action='store_false',
